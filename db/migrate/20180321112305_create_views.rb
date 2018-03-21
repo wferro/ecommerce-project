8c8339ac03ee2e447feed857b50a261728c323e1
Wellington Ferro <w.ferro2@gmail.com>
2018-03-21 11:23:21 +0000

    Adding View model

---
+++ db/migrate/20180321112305_create_views.rb
@@ -0,0 +1,10 @@
+class CreateViews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :views do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
