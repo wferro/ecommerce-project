b745310436e246d830b4503c65e2a04a7a8a88b5
Wellington Ferro <w.ferro2@gmail.com>
2018-03-21 11:23:56 +0000

    Migrating View model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320134232) do
+ActiveRecord::Schema.define(version: 20180321112305) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -74,6 +74,17 @@ ActiveRecord::Schema.define(version: 20180320134232) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "total"
+    t.integer "order_id"
+    t.integer "brigadeiro_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brigadeiro_id"], name: "index_order_items_on_brigadeiro_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.datetime "date"
     t.string "paymentInfo"
@@ -98,6 +109,13 @@ ActiveRecord::Schema.define(version: 20180320134232) do
     t.string "name"
     t.string "email"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
