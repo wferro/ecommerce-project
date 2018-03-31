6f152be94c207665b43c6e8153d9e7f6aeed3544
Wellington Ferro <w.ferro2@gmail.com>
2018-03-31 17:06:53 +0000

    running migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331170413) do
+ActiveRecord::Schema.define(version: 20180331170548) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -94,6 +94,8 @@ ActiveRecord::Schema.define(version: 20180331170413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "pst"
+    t.integer "gst"
+    t.integer "hst"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
