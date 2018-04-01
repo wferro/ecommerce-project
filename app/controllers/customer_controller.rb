8f29be2266153fcec5782531f6e15b9adb451c06
Wellington Ferro <w.ferro2@gmail.com>
2018-04-01 21:25:08 +0000

    Fixing Rubcop

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -48,7 +48,7 @@ class CustomerController < ApplicationController
 
     total = 0
     @cartItems.each do |i|
-			total = total + i['subtotal'].to_f
+      total = total + i['subtotal'].to_f
 		end
 
     order = Order.create(
@@ -66,34 +66,27 @@ class CustomerController < ApplicationController
         brigadeiro: brigadeiro,
         order: order)
   	end
-
     session[:new_order] = Order.find(order.id)
-
   end
 
   def order
     Stripe.api_key = "sk_test_hchL9UYFMBH7AbndbI7ukDmU"
     charge = Stripe::Charge.create({
-    	statement_descriptor: 'Chocoball Store',
-        amount: session[:new_order]['total'] + session[:new_order]['pst'] + session[:new_order]['gst'],
-        currency: 'cad',
-        source: 'tok_visa',
-        receipt_email: 'wferro@rrc.ca',
+      statement_descriptor: 'Chocoball Store',
+      amount: session[:new_order]['total'] + session[:new_order]['pst'] + session[:new_order]['gst'],
+      currency: 'cad',
+      source: 'tok_visa',
+      receipt_email: 'wferro@rrc.ca',
     })
-
 # charge = Stripe::Charge.create({statement_descriptor: 'Chocoball Store',amount: 123,currency: 'cad',source:
 # {exp_month: '10',exp_year: '20',number: '4012888888881881',object: 'card',cvc: '123'},receipt_email: 'wferro@rrc.ca'})
 # Stripe::CardError: (Status 402) (Request req_MSFcHp6F991Cop) Sending credit card numbers directly to the Stripe API
 # is generally unsafe. We suggest you use test tokens that map to the test card you are using, see https://stripe.com/docs/testing.
-
     order = Order.find(session[:new_order]['id'])
     order.paymentInfo = charge['id']
     order.status = Status.where("name = 'ACCEPTED'").first
     order.save
-
     flash[:success] = "Order FINALLY placed. Payment  #{order.paymentInfo}  succesfuly accepted"
-    puts flash[:success]
-
   end
 
   def load_provinces
